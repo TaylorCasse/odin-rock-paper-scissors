a646cea6418020e680082abed40294ce8c890212
Taylor Casse <taylorcasse@gmail.com>
2022-07-26 13:29:17 +0200

    Added win conditions and basic multi-winner handling

--- index.js
+++ index.js
@@ -99,18 +99,6 @@ function winCheck(playerInput, compInput) {
 
 // Assigning DOM elements
 const gameScreen = document.querySelector('#game-container');
-// const playerScoreCard = document.querySelector('#player-score');
-// const playerScoreBlocks = playerScoreCard.children;
-// const opponentScoreCard = document.querySelector('#opponent-score');
-// const opponentScoreBlocks = opponentScoreCard.children;
-// const roundsDisplay = document.querySelector('#current-round');
-// const resultElement = document.querySelector('#result-text');
-// const mainContainer = document.querySelector('#game-container');
-
-// const rockCard = document.querySelector('#player-rock');
-// const paperCard = document.querySelector('#player-paper');
-// const scissorsCard = document.querySelector('#player-scissors');
-
 
 function Card(type) {
     this.type = type;
@@ -148,7 +136,6 @@ function Player(name, isBot) {
 
     this.scoreElement = document.createElement('div');
     this.scoreElement.setAttribute('class', 'score-tracker');
-    const winsToWin = 3;
     for (let i = 0; i < winsToWin; i++) {
         const scoreBlock = document.createElement('div');
         scoreBlock.setAttribute('class', 'score-block');    
@@ -206,22 +193,14 @@ function Player(name, isBot) {
         return activeList[randIntFromInterval(0, activeList.length - 1)];
     };
 
-    this.botSanitize = function() {
-        for (let card of this.hand) {
-            // card.removeEventListener('click', playRound);
-            card.element.classList.remove('hover-transition');
-            console.log(card.element);
-        }
-
-    }
-
 }
 
 
 // Game set-up
 switch (gameMode) {
     case 'First to 3':
-        numberOfBots = 9
+        numberOfBots = 9;
+        winsToWin = 3;
 }
 
 
@@ -240,7 +219,6 @@ for (let i = 0; i < numberOfBots; i++) {
     let randName = botNames[botNameIndex];      // Get a random bot name
     botNames.splice(botNameIndex, 1);           // Ensure unique names
     let newBot = new Player(randName, true);
-    newBot.botSanitize();
     gameScreen.appendChild(newBot.element);
     botList.push(newBot);
 }
@@ -315,44 +293,33 @@ function playRound(playerChoice) {
     }
     console.log(highestRoundScore)
 
+    const winners = [];
+    let winnerFound = false;
     for (let player of playerList) {
+        // Finds the winner(s) of the round
         if (player.roundScore === highestRoundScore && player.roundScore > 0) {
-            player.winRound()
+            player.winRound();
+            // Finds winner(s) of the game
+            if (player.score === winsToWin) {
+                winnerFound = true;
+                winners.push(player);
+            }
         }
-    }
-    
-    const compChoiceVar = botList[0].autoChoice();
-    const winStatus = winCheck(playerChoice, compChoiceVar);
 
-    if (winStatus === 'Win') {
-        playerWins += 1;
-        for (let i=0; i < playerWins; i++) {
-            playerScoreBlocks[i].classList.add('score-block-filled');
-        }
-        resultElement.textContent = "You win!";
-    } else if (winStatus === 'Lose') {
-        compWins += 1;
-        for (let i=0; i < compWins; i++) {
-            opponentScoreBlocks[i].classList.add('score-block-filled');
-        }
-        resultElement.textContent = "You lose...";
-    } else {
-        resultElement.textContent = 'Draw';
     }
 
-    if (playerWins === 3 || compWins === 3) {
-        gameScreen.textContent = 'GAME OVER';
-        const reloadButton = document.createElement('button');
-        reloadButton.setAttribute('id', 'reload-button');
-        reloadButton.textContent = 'Play again';
-        reloadButton.addEventListener('click', () => {
-            location.reload();
-        })
-        resultElement.appendChild(reloadButton);
-    } else {
-        currentRound += 1;
-        roundsDisplay.textContent = `Round ${currentRound}`;
+    if (winners.length === 1) {
+        gameScreen.textContent = `${winners[0].name} wins!`;
+        // for (winner )
+    } else if (winners.length === 2) {
+        gameScreen.textContent = `${winners[0].name} and ${winners[1].name} win!`;
+        for (let winner of winners) {
+            console.log(winner);
+        }
+        
     }
+
+
 
 }
 
@@ -364,38 +331,5 @@ function gameOverScreen(playerWin) {
         gameScreen.textContent = 'Game over\nYou lose...';
     }
 }
-
-
-// function playAnimations(playerChoice, botChoice) {
-//     const cards = [rockCard, paperCard, scissorsCard];
-//     let playerCard;
-//     switch (playerChoice) {
-//         case 'Rock':
-//             playerCard = rockCard;
-//         case 'Paper':
-//             playerCard = paperCard;
-//         case 'Scissors':
-//             playerCard = scissorsCard;
-//     }
-// }
-
-// rockCard.addEventListener('click', () => {
-//     console.log("Rock pressed");
-//     playRound('Rock');
-//     playerRock.chosen();
-// })
-
-
-// paperCard.addEventListener('click', () => {
-//     console.log("Paper pressed");
-//     playRound('Paper');
-// })
-
-// scissorsCard.addEventListener('click', () => {
-//     console.log("Scissors pressed");
-//     playRound('Scissors');
-// })
-
-
 
 // template https://www.crazygames.com/game/rock-paper-scissors
