15288c98ce654689a54200fb94ef0cfa730b2f35
Taylor Casse <taylorcasse@gmail.com>
2022-07-28 16:43:42 +0200

    Implimented enlarge and fade, need to impliment animations

--- index.js
+++ index.js
@@ -90,14 +90,7 @@ function winCheck(playerInput, compInput) {
     }
 }
 
-// // Declaring global variables 
-// let playerWins = 0;
-// let compWins = 0;
 
-
-// Assigning DOM elements
-const gameScreen = document.querySelector('#game-container');
-const roundTracker = document.querySelector('#current-round');
 
 function Card(type) {
     this.type = type;
@@ -128,7 +121,7 @@ function Player(name, isBot) {
 
     // Generate DOM Elements
     this.element = document.createElement('div');
-    this.element.setAttribute('class', 'player-container');
+    this.element.setAttribute('class', 'container');
 
     const heading = document.createElement('h3');
     heading.textContent = `${this.name.toUpperCase()}'S SCORE`;
@@ -151,13 +144,15 @@ function Player(name, isBot) {
         // if the card is owned by a human player
         if (!this.isBot) {
             currentCard.element.classList.toggle('hover-transition');
+            currentCard.element.classList.toggle('player-card');
             currentCard.element.addEventListener('click', () => {
-                playRound(currentCard.type);
+                player.choice = currentCard.type;
+                playRound(this);
             })
             currentCard.reset = function() {
                 this.element.removeAttribute('class');
                 this.element.classList.toggle('card');
-                this.element.classList.toggle('hover-transition');
+                this.element.classList.toggle('player-card');
             }
         } else {
             currentCard.reset = function() {
@@ -181,6 +176,21 @@ function Player(name, isBot) {
         }
     }
 
+    this.playAnimations = function() {
+        for (let card of this.hand) {
+            if (card.type === this.choice) {
+                card.enlarge();
+            } else {
+                card.fade()
+            }
+        }
+        setTimeout(() => {
+            for (let card of this.hand) {
+                card.reset();
+            }
+        }, 1000);
+    }
+
     this.reset = function() {
         this.score = 0;
         for (let i = 0; i < this.scoreElement.children.length; i++) {
@@ -201,43 +211,25 @@ switch (gameMode) {
     case 'First to 3':
         // numberOfBots = botNames.length;
         numberOfBots = 15;
-
         winsToWin = 3;
 }
 
 
 
-const playerName = 'Taylor';
-const player = new Player(playerName, false);
-const botList = [];
-gameScreen.appendChild(player.element);
-document.addEventListener('keydown', () => {
-    player.reset();
-})
 
-for (let i = 0; i < numberOfBots; i++) {
-    const botNameIndex = randIntFromInterval(0, botNames.length - 1)
-    const randName = botNames[botNameIndex];      // Get a random bot name
-    // botNames.splice(botNameIndex, 1);           // Ensure unique names
-    const newBot = new Player(randName, true);
-    gameScreen.appendChild(newBot.element);
-    botList.push(newBot);
-}
-
-let currentRound = 1;
-function playRound(playerChoice) {
+function playRound(player) {
     
-    player.choice = playerChoice;
-    playerList = [player];
 
-    // Bots make their choice, generate array of all players with choices
+    const playerList = [player];
+    player.playAnimations();
+    // Bots make their choice, generate array of all player's choices
     for (let bot of botList) {
-        bot.choice = bot.autoChoice()
+        bot.choice = bot.autoChoice();
+        bot.playAnimations();
         playerList.push(bot);
     }
 
     // Compares the choices of all players and adds up each players wins (round score) in this round
-    console.log('Round')
     for (let i = 0; i < playerList.length; i++) {
         playerList[i].roundScore = 0;
         for (let j = 0; j < playerList.length; j++) {
@@ -251,38 +243,30 @@ function playRound(playerChoice) {
     // Finds the player with the highest round score
     let highestRoundScore = 0;
     for (let player of playerList) {
-        console.log(`${player.name}'s choice: ${player.choice}`);
+        // console.log(`${player.name}'s choice: ${player.choice}`);
         if (player.roundScore > highestRoundScore) {
             highestRoundScore = player.roundScore;
         }
     }
-    console.log(highestRoundScore)
 
     const winners = [];
-    let winnerFound = false;
     for (let player of playerList) {
         // Finds the winner(s) of the round
         if (player.roundScore === highestRoundScore && player.roundScore > 0) {
             player.winRound();
             // Finds winner(s) of the game
             if (player.score === winsToWin) {
-                winnerFound = true;
                 winners.push(player);
             }
         }
-
     }
 
     currentRound += 1;
     roundTracker.textContent = `Round ${currentRound}`;
 
-
-    if (winnerFound) {
+    if (winners) {
         gameOver(winners);
     }
-
-
-
 }
 
 
@@ -304,5 +288,48 @@ function gameOver(winners) {
         }
     }
 }
+
+
+const mainContainer = document.querySelector('#main');
+function initialize() {
+    gameScreen.textContent = '';
+
+    mainHeading.textContent = 'TOP Rock Paper Scissors';
+
+
+    roundTracker.textContent = 'Round 1';
+    roundTracker.classList.toggle('current-round');
+
+    mainContainer.appendChild(mainHeading);
+    mainContainer.appendChild(roundTracker);
+
+
+
+    gameScreen.appendChild(player.element);
+    document.addEventListener('keydown', () => {
+        player.reset();
+    })
+    
+    
+    for (let i = 0; i < numberOfBots; i++) {
+        const botNameIndex = randIntFromInterval(0, botNames.length - 1)
+        const randName = botNames[botNameIndex];      // Get a random bot name
+        // botNames.splice(botNameIndex, 1);           // Ensure unique names
+        const newBot = new Player(randName, true);
+        gameScreen.appendChild(newBot.element);
+        botList.push(newBot);
+    }
+    
+}
+// Assigning DOM elements
+const gameScreen = document.querySelector('#game-container');
+const playerName = 'Taylor';
+const player = new Player(playerName, false);
+const botList = [];
+let currentRound = 1;
+const roundTracker = document.createElement('h2');
+const mainHeading = document.createElement('h1');
+
+initialize()
 
 // template https://www.crazygames.com/game/rock-paper-scissors
